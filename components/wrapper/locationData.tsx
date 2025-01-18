92268f35c956e37e32d6540816c2abb1f2457acb
Uday Jawheri <jawheriuday@gmail.com>
2025-01-18 15:24:05 +0530

    navbar updated

---
+++ components/wrapper/locationData.tsx
@@ -0,0 +1,42 @@
+export const countries = [
+    "Australia",
+    "Canada",
+    "France",
+    "Germany",
+    "India",
+    "Italy",
+    "Netherlands",
+    "Spain",
+    "United Kingdom",
+    "United States",
+    ]
+    
+    export const cities = [
+    "Sydney",
+    "Toronto",
+    "Chicago",
+    "Dallas",
+    "Houston",
+    "Los Angeles",
+    "San Diego",
+    "London",
+    "Manchester",
+    "Melbourne",
+    "Mumbai",
+    ];
+    
+    export const states = [
+    "Arizona",
+    "California",
+    "Colorado",
+    "Florida",
+    "Georgia",
+    "Illinois",
+    "Massachusetts",
+    "Minnesota",
+    "New York",
+    "Pennsylvania",
+    "Texas",
+    "Virginia",
+    "Washington",
+    ]
