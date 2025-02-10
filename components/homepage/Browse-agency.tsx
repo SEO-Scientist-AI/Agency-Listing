3bb7f4eec554c04c43509d49ee1d59a56f390289
Uday Jawheri <jawheriuday@gmail.com>
2025-02-10 14:09:13 +0530

    Update Browse-agency.tsx

--- components/homepage/Browse-agency.tsx
+++ components/homepage/Browse-agency.tsx
@@ -69,9 +69,9 @@ const BrowseAgencyGalleries = () => {
                     We simplify every step of your research process
                 </h2>
                 <p className="mx-auto max-w-[500px] text-gray-600 dark:text-gray-400 text-center mt-2 ">
-                    Whatever your search criteria are, we&apos;ve got you covered.
-                    Find a suitable agency team in one of these dedicated
-                    galleries.
+                    Whatever your search criteria are, we&apos;ve got you
+                    covered. Find a suitable agency team in one of these
+                    dedicated galleries.
                 </p>
             </div>
             <div className="w-full">
@@ -79,48 +79,41 @@ const BrowseAgencyGalleries = () => {
                     <TabsList className="flex flex-wrap   w-full justify-center border-border mb-16 sm:mb-6 bg-white dark:bg-black">
                         <TabsTrigger
                             value="service"
-
                             className="inline font-semibold dark:text-gray-100 text-gray-900 border-b-2 border-transparent px-4 py-2 data-[state=active]:border-[#FF4405] data-[state=active]:text-gray-900 dark:data-[state=active]:text-white data-[state=active]:shadow-none hover:text-[#ff642d] hover:border-[#FF4405]"
                         >
                             By service
                         </TabsTrigger>
                         <TabsTrigger
                             value="industry"
-
                             className="inline font-semibold dark:text-gray-100 text-gray-900 border-b-2 border-transparent px-4 py-2 data-[state=active]:border-[#FF4405] data-[state=active]:text-gray-900 dark:data-[state=active]:text-white data-[state=active]:shadow-none hover:text-[#ff642d] hover:border-[#FF4405]"
                         >
                             By industry
                         </TabsTrigger>
                         <TabsTrigger
                             value="country"
-
                             className="inline font-semibold dark:text-gray-100 text-gray-900 border-b-2 border-transparent px-4 py-2 data-[state=active]:border-[#FF4405] data-[state=active]:text-gray-900 dark:data-[state=active]:text-white data-[state=active]:shadow-none hover:text-[#ff642d] hover:border-[#FF4405]"
                         >
                             By country
                         </TabsTrigger>
-                        <TabsTrigger
+                        {/* <TabsTrigger
                             value="state"
 
                             className="inline font-semibold dark:text-gray-100 text-gray-900 border-b-2 border-transparent px-4 py-2 data-[state=active]:border-[#FF4405] data-[state=active]:text-gray-900 dark:data-[state=active]:text-white data-[state=active]:shadow-none hover:text-[#ff642d] hover:border-[#FF4405]"
                         >
                             By state
-                        </TabsTrigger>
+                        </TabsTrigger> */}
                         <TabsTrigger
                             value="city"
-
                             className="inline font-semibold dark:text-gray-100 text-gray-900 border-b-2 border-transparent px-4 py-2 data-[state=active]:border-[#FF4405] data-[state=active]:text-gray-900 dark:data-[state=active]:text-white data-[state=active]:shadow-none hover:text-[#ff642d] hover:border-[#FF4405]"
                         >
                             By city
                         </TabsTrigger>
-                        
-
                     </TabsList>
                     <TabContent tabData={services} value="service" />
                     <TabContent tabData={industries} value="industry" />
-                    <TabContent tabData={countries} value="country"/>
-                    <TabContent tabData={states} value="state"/>
-                    <TabContent tabData={cities} value="city"/>
-                    
+                    <TabContent tabData={countries} value="country" />
+                    <TabContent tabData={states} value="state" />
+                    <TabContent tabData={cities} value="city" />
 
                     {/* Other TabsContent components can be added here for service, country, state, city, and size */}
                 </Tabs>
